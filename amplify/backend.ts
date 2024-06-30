016c49073a6983536ec1826355ac90781613b883
tm-hirai <tm-hirai@kddi.com>
2024-06-30 22:39:03 +0900

    不要なファイルを削除

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource";
 import { data, MODEL_ID, calorieCalculationFunction } from "./data/resource";
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export const backend = defineBackend({
-  auth,
   data,
   calorieCalculationFunction,
 });
